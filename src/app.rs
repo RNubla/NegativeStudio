d284be636030fae525d74e24859b0d54d606f125
Robert Nubla <rnubla97@gmail.com>
2022-07-23 15:48:40 -0400

    Fetched image from url to be previewed

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,16 @@
+use egui_extras::RetainedImage;
+use image::{imageops, GenericImageView, ImageBuffer, RgbImage};
+use poll_promise::Promise;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
     // Example stuff:
     label: String,
+    url: String,
+    #[serde(skip)] // don't serialize this
+    promise: Option<Promise<ehttp::Result<RetainedImage>>>,
 
     // this how you opt-out of serialization of a member
     #[serde(skip)]
@@ -16,6 +23,8 @@ impl Default for TemplateApp {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            url: "".to_owned(),
+            promise: None,
         }
     }
 }
@@ -45,7 +54,12 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let Self {
+            label,
+            value,
+            url,
+            promise: _,
+        } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -56,6 +70,9 @@ impl eframe::App for TemplateApp {
             // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
+                    if ui.button("Open Image").clicked() {
+                        println!("Open Image");
+                    }
                     if ui.button("Quit").clicked() {
                         frame.quit();
                     }
@@ -70,6 +87,8 @@ impl eframe::App for TemplateApp {
                 ui.label("Write something: ");
                 ui.text_edit_singleline(label);
             });
+
+            // if ui.button("Load Image").clicked(){}
 
             ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
             if ui.button("Increment").clicked() {
@@ -86,8 +105,49 @@ impl eframe::App for TemplateApp {
                 });
             });
         });
-
+        // egui::CentralPanel::default().show(ctx, |ui| match promise.ready() {
+        //     None => {
+        //         ui.spinner(); // still loading
+        //     }
+        //     Some(Err(err)) => {
+        //         ui.colored_label(egui::Color32::RED, err); // something went wrong
+        //     }
+        //     Some(Ok(image)) => {
+        //         image.show_max_size(ui, ui.available_size());
+        //         egui::warn_if_debug_build(ui);
+        //     }
+        // });
         egui::CentralPanel::default().show(ctx, |ui| {
+            let trigger_fetch = ui_url(ui, url);
+
+            if trigger_fetch {
+                let ctx = ctx.clone();
+                let (sender, promise) = Promise::new();
+                let request = ehttp::Request::get(url);
+                ehttp::fetch(request, move |response| {
+                    let image = response.and_then(parse_response);
+                    // let img = image::open(image).expect("Failed to load image");
+                    sender.send(image); // send the results back to the UI thread.
+                    ctx.request_repaint(); // wake up UI thread
+                });
+                self.promise = Some(promise);
+            }
+
+            if let Some(promise) = &self.promise {
+                match promise.ready() {
+                    None => {
+                        ui.spinner(); // still loading
+                    }
+                    Some(Err(err)) => {
+                        ui.colored_label(egui::Color32::RED, err); // something went wrong
+                    }
+                    Some(Ok(image)) => {
+                        image.show_max_size(ui, ui.available_size());
+                        egui::warn_if_debug_build(ui);
+                    }
+                }
+            }
+
             // The central panel the region left after adding TopPanel's and SidePanel's
 
             ui.heading("eframe template");
@@ -107,5 +167,31 @@ impl eframe::App for TemplateApp {
                 ui.label("You would normally chose either panels OR windows.");
             });
         }
+    }
+}
+
+fn ui_url(ui: &mut egui::Ui, url: &mut String) -> bool {
+    let mut trigger_fetch = false;
+    ui.horizontal(|ui| {
+        ui.label("URL:");
+        trigger_fetch |= ui
+            .add(egui::TextEdit::singleline(url).desired_width(f32::INFINITY))
+            .lost_focus();
+    });
+    trigger_fetch
+}
+
+// fn ui_url
+
+#[allow(clippy::needless_pass_by_value)]
+fn parse_response(response: ehttp::Response) -> Result<RetainedImage, String> {
+    let content_type = response.content_type().unwrap_or_default();
+    if content_type.starts_with("image/") {
+        RetainedImage::from_image_bytes(&response.url, &response.bytes)
+    } else {
+        Err(format!(
+            "Expected image, found content-type {:?}",
+            content_type
+        ))
     }
 }
